7f6034e1902535d91220ca7ae2678fd644279043
Shazdeh <hassan.derakhshandeh@gmail.com>
2026-01-01 12:49:52 +0330

    Create plugin.cpp

---
+++ plugin.cpp
@@ -0,0 +1,73 @@
+﻿#include <unordered_set>
+
+PlayerCharacter* player;
+TESGlobal* countGlobal;
+BGSKeyword* fortifyHealth;
+bool bTaskQueued = false;
+
+void UpdateDiseaseCount() {
+    int count = 0;
+    const auto& effects = player->AsMagicTarget()->GetActiveEffectList();
+    std::unordered_set<const MagicItem*> seenSpells;
+    for (auto it = effects->begin(); it != effects->end(); ++it) {
+        auto& value = *it;
+        if (value->spell && value->spell->GetSpellType() == MagicSystem::SpellType::kDisease) {
+            if (seenSpells.insert(value->spell).second) {
+                count++;
+            }
+        }
+    }
+
+    countGlobal->value = count;
+    bTaskQueued = false;
+}
+
+void MaybeUpdateDiseaseCount() {
+    if (bTaskQueued) return;
+    bTaskQueued = true;
+    SKSE::GetTaskInterface()->AddTask(UpdateDiseaseCount);
+}
+
+class theSink : public BSTEventSink<TESMagicEffectApplyEvent> {
+    BSEventNotifyControl ProcessEvent(const TESMagicEffectApplyEvent* event,
+                                      BSTEventSource<TESMagicEffectApplyEvent>*) {
+        if (event->target && event->target->IsPlayerRef()) {
+            MaybeUpdateDiseaseCount();
+        }
+        return BSEventNotifyControl::kContinue;
+    }
+};
+
+bool IsHealthModifierEffect(EffectSetting* a_effect) {
+    auto type = a_effect->GetArchetype();
+    if ((type == EffectArchetypes::ArchetypeID::kPeakValueModifier ||
+            type == EffectArchetypes::ArchetypeID::kValueModifier ||
+            type == EffectArchetypes::ArchetypeID::kDualValueModifier)
+        && (a_effect->data.primaryAV == ActorValue::kHealth || a_effect->data.secondaryAV == ActorValue::kHealth)
+    ) {
+        return true;
+    }
+    return false;
+}
+
+SKSEPluginLoad(const SKSE::LoadInterface* skse) {
+    SKSE::Init(skse);
+
+    SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message* message) {
+        if (message->type == SKSE::MessagingInterface::kDataLoaded) {
+        } else if (message->type == SKSE::MessagingInterface::kPostLoadGame ||
+                   message->type == SKSE::MessagingInterface::kNewGame) {
+            static bool init = false;
+            if (init) return;
+            countGlobal = TESForm::LookupByEditorID<TESGlobal>("LoreTraits_PlayerDiseaseCount");
+            if (!countGlobal) return;
+            // fortifyHealth = TESForm::LookupByEditorID<BGSKeyword>("LoreTraits_FortifyHealthEffect");
+            player = RE::PlayerCharacter::GetSingleton();
+            static theSink g_sink;
+            ScriptEventSourceHolder::GetSingleton()->AddEventSink(&g_sink);
+            init = true;
+        }
+    });
+
+    return true;
+}
